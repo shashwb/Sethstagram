2cba10dc66f18c2b8639ca924a775afb9377b50b
Seth Balodi <shashwb@vt.edu>
2017-09-12 21:59:13 -0400

    second

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+# this will be the main controller of the application
+# will control all of our posts and will hold our initial route
+class PostsController < ApplicationController
+
+  def inded
+    # current does nothing
+  end
+
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+# think of a model as just the window in a drivethrough to the database
+# currently, this model is empty, meaning there is nobody there to even take my order
+class Post < ActiveRecord::Base
+end
