35fbcc6557d4901c5e3ea172a80b94e07cb1b939
Seth Balodi <shashwb@vt.edu>
2017-09-13 20:48:23 -0400

    yah

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 # think of a model as just the window in a drivethrough to the database
 # currently, this model is empty, meaning there is nobody there to even take my order
 class Post < ActiveRecord::Base
+
+  # so basically this REQUIRES that there be an image
+  validates :image, presence: true
+
+  #required by paperclip
+  has_attached_file :image, style: { :medium => "640px" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+
 end
